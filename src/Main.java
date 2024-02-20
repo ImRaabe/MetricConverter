8d4fe2ce9ea970e1e3c1d8733a113ff42edae83f
ImRaabe <raabees@mail.uc.edu>
2024-02-20 00:22:34 -0500

    Project init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double meters;
+        do {
+            System.out.print("Please enter a measurement in meters: ");
+            while (!in.hasNextDouble()) {
+                System.out.print("Invalid input. Please enter a valid number: ");
+                in.next();
+            }
+            meters = in.nextDouble();
+        } while (meters <= 0);
+
+        double miles = meters * 0.000621371;
+        double feet = meters * 3.28084;
+        double inches = meters * 39.3701;
+
+        System.out.println(meters + " meters is equal to:");
+        System.out.println(miles + " miles");
+        System.out.println(feet + " feet");
+        System.out.println(inches + " inches");
+
+        in.close();
     }
 }
